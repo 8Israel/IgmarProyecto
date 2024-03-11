1537f44b0d1b39104720c03fd745b88d9fdf49d5
Daniel Fong <danifongm@gmail.com>
2024-03-11 12:41:43 -0600

    Cambios a pantallas

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -5,7 +5,9 @@ import { authGuard } from './guards/auth.guard';
 
 
 export const routes: Routes = [
-    {path: '', loadComponent: () => import('./components/verficar-login/verficar-login.component').then(v => v.VerficarLoginComponent)},
-    {path: 'register', loadComponent: () => import('./components/register/register.component').then(r => r.RegisterComponent) },
+    {path: '', loadComponent: () => import('./components/login/login.component').then(l => l.LoginComponent)},
+    {path: 'verificar', loadComponent: () => import('./components/verficar-login/verficar-login.component').then(v => v.VerficarLoginComponent)},
+    {path: 'registrar', loadComponent: () => import('./components/register/register.component').then(r => r.RegisterComponent) },
+    {path: 'registrar-jugador', loadComponent: () => import('./components/registrar-jugador/registrar-jugador.component').then(rj => rj.RegistrarJugadorComponent)},
     {path: 'dashboard', loadComponent: () => import('./components/dashboard/dashboard.component').then(d => d.DashboardComponent), canActivate: [authGuard]},
 ];
