9d6134bb808c1e22ccc25411e1adc7aaa6e8cd0a
Israel De la rosa <juanisraeldelarosa753@gmail.com>
2024-03-12 17:26:59 -0600

    Controador amigos y rutas ordenada segun 2.0

--- api/database/seeders/RolesTableSeeder.php
+++ api/database/seeders/RolesTableSeeder.php
@@ -13,14 +13,8 @@ class RolesTableSeeder extends Seeder
      */
     public function run(): void
     {
-<<<<<<< HEAD
          Role::create(['name' => 'admin']);
          Role::create(['name' => 'user']);
          Role::create(['name' => 'guest']);
-=======
-        Role::create(['name' => 'admin']);
-        Role::create(['name' => 'user']);
-        Role::create(['name' => 'guest']);
->>>>>>> 29479a3aeb28df7ffd6472111937f08369130abc
     }
 }
