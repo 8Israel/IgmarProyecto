b9ad1a356d1ba601dd61c3213c81d90782d8fa2f
Dfong1 <103082807+Dfong1@users.noreply.github.com>
2024-03-12 19:36:09 -0600

    merge en migracion de inventariosjugador arreglao

--- api/database/migrations/2024_03_07_182030_inventarios_jugador.php
+++ api/database/migrations/2024_03_07_182030_inventarios_jugador.php
@@ -10,19 +10,9 @@ return new class extends Migration
     {
         Schema::create('inventarios_users', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD
-
-           
-
-            $table->foreignId('user_id')->references('id')->on('users');
-            $table->foreignId('armas_id')->references('id')->on('armas');
-            $table->foreignId('heroes_id')->references('id')->on('heroes');
-
-=======
             $table->foreignId('user_id')->nullable()->references('id')->on('users');
             $table->foreignId('armas_id')->nullable()->references('id')->on('armas');
             $table->foreignId('heroes_id')->nullable()->references('id')->on('heroes');
->>>>>>> 45f4548c779ea3f9148ae3017d43a31caa16ea14
             $table->timestamps();
         });
     }
