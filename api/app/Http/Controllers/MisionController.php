12049ea1ab6dc253934dcead14281b73e90938ae
Israel De la rosa <juanisraeldelarosa753@gmail.com>
2024-03-18 19:54:13 -0600

    logs de mongo completos

--- api/app/Http/Controllers/MisionController.php
+++ api/app/Http/Controllers/MisionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Logs;
 use App\Models\Mision;
 use App\Models\User;
 use App\Models\Recompensa;
@@ -11,9 +12,12 @@ use Validator;
 
 class MisionController extends Controller
 {
-    public function index($id = null)
+    public function index(Request $request, $id = null)
     {
-        $misiones = Mision::with('recompensa')->get();
+        $query = Mision::with('recompensa');
+        $sqlQuery = $query->toSql();
+        $misiones = $query->get();
+        $this->LogsMethod($request, auth()->user(), $sqlQuery);
         return response()->json($misiones);
     }
 
@@ -28,15 +32,16 @@ class MisionController extends Controller
             return response()->json(['error' => $validator->errors()->all()], 404);
         }
         if (!Recompensa::where('id', $request->recompensas_id)->exists()) {
-            return response()->json(['error'=> 'la recompensa indicada no existe'],404);
+            return response()->json(['error' => 'la recompensa indicada no existe'], 404);
         }
         $mision = Mision::create($request->all());
+        $this->LogsMethod($request, auth()->user());
         return response()->json(['message' => 'Mision creada correctamente', 'data' => $mision], 201);
     }
     public function update(Request $request, $id)
     {
         if (!Mision::where('id', $id)->exists()) {
-            return response()->json(['error'=> 'La mision indicada no existe'],404);
+            return response()->json(['error' => 'La mision indicada no existe'], 404);
         }
         $validator = Validator::make($request->all(), [
             "nombre" => 'string|min:3|max:20|unique:misiones',
@@ -46,22 +51,36 @@ class MisionController extends Controller
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()->all()], 404);
         }
-        if ($request->has('recompensas_id') && !Recompensa::where('id', $request->recompensas_id)->exists())
-        {
-            return response()->json(['error'=> 'la recompensa indicada no existe'],404);
+        if ($request->has('recompensas_id') && !Recompensa::where('id', $request->recompensas_id)->exists()) {
+            return response()->json(['error' => 'la recompensa indicada no existe'], 404);
         }
+        $this->LogsMethod($request, auth()->user(), ["modificado"=>$request->all(), "mision"=>$id]);
         $mision = Mision::findOrFail($id);
         $mision->update($request->all());
         return response()->json(['message' => 'Mision actualizada correctamente', 'data' => $mision], 200);
     }
 
-    public function destroy($id)
+    public function destroy(Request $request,$id)
     {
         if (!Mision::where('id', $id)->exists()) {
-            return response()->json(['error'=> 'La mision indicada no existe'],404);
+            return response()->json(['error' => 'La mision indicada no existe'], 404);
         }
         $mision = Mision::findOrFail($id);
+        $this->LogsMethod($request, auth()->user(), $mision->toArray() );
         $mision->delete();
-        return response()->json(['message' => 'Mision eliminada correctamente'],200);
+        return response()->json(['message' => 'Mision eliminada correctamente'], 200);
+    }
+    public function LogsMethod(Request $request, $user, $query = null)
+    {
+        if (!$query) {
+            $data = $request->all();
+        } else {
+            $data = $query;
+        }
+        Logs::create([
+            "user_id" => $user->id,
+            "data" => $data,
+            "verb" => $request->method(),
+        ]);
     }
 }
