b12401360afa41cc3a3ee82d432575422c780fb2
Israel De la rosa <juanisraeldelarosa753@gmail.com>
2024-04-08 16:32:07 -0600

    new

---
+++ api/app/Http/Controllers/testwebsoket.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\NuevaMision;
+use Illuminate\Http\Request;
+
+class testwebsoket extends Controller
+{
+    public function test(){
+        event(new NuevaMision);
+    }
+}
--- api/routes/web.php
+++ api/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MisionController;
+use App\Http\Controllers\testwebsoket;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', [testwebsoket::class,'test']);//Mostrar todas las misiones, User, Admin,Guest
+
